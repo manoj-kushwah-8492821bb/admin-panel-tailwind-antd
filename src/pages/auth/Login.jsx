7562e8dabbcb080fa5a09f23009b761852fd1015
Manoj Kushwah <manojkushwah1066@gmail.com>
2023-08-30 16:09:00 +0530

    login with otp

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -3,7 +3,6 @@ import Logo from "../../assets/Logo.png";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { authLogin } from "../../toolkit/action/authAction";
-import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import ButtonLoader from "../../common/ButtonLoader";
 import SimpleReactValidator from "simple-react-validator";
 import ShowError from "../../common/ShowError";
@@ -12,10 +11,23 @@ const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [errors, setErrors] = useState({});
-  const [show, setShow] = useState(false);
+  const [isOtp, SetIsOtp] = useState(false);
+
   const [formInput, setFormInput] = useState({});
   const { loading } = useSelector((state) => state.authReducer);
-  const validator = new SimpleReactValidator({ className: "text-danger" });
+  const validator = new SimpleReactValidator({
+    validators: {
+      number: {
+        message: "The :attribute must be a number.",
+        rule: function (val, params, validator) {
+          return (
+            validator.helpers.testRegex(val, /^\d+$/) &&
+            params.indexOf(val) === -1
+          );
+        },
+      },
+    },
+  });
 
   // handleChange
   const handleChange = (event) => {
@@ -28,7 +40,8 @@ const Login = () => {
     event.preventDefault();
     if (validator.allValid()) {
       const response = await dispatch(authLogin(formInput));
-      if (response?.payload?.Status) {
+      response?.payload?.ResponseStatus == 3 && SetIsOtp(true);
+      if (response?.payload?.ResponseStatus == 2) {
         navigate("/dashboard");
         window.location.reload();
       }
@@ -46,71 +59,69 @@ const Login = () => {
           backgroundImage: `url(
               "https://images.unsplash.com/photo-1498736297812-3a08021f206f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=879&q=80"
             )`,
-        }}></div>
+        }}
+      ></div>
 
       <form
         onSubmit={handleSubmit}
-        className="max-w-lg mx-auto itemc w-full px-6 py-8 flex flex-col justify-center md:px-8 lg:w-1/3">
+        className="max-w-lg mx-auto itemc w-full px-6 py-8 flex flex-col justify-center md:px-8 lg:w-1/3"
+      >
         <div className="flex justify-center mx-auto">
           <img className="w-auto h-10 sm:h-12" src={Logo} alt="" />
         </div>
 
         <p className="mt-3 text-xl text-center  ">Welcome back</p>
 
-        {/* Email */}
+        {/* phone */}
         <div className="mt-4">
-          <label className="block mb-2 text-sm font-medium  " htmlFor="email">
-            Email Address
+          <label className="block mb-2 text-sm font-medium  " htmlFor="phone">
+            Phone
           </label>
           <input
             autoComplete="off"
-            id="email"
+            id="phone"
             className="block w-full px-4 py-2 bg-transparent border-gray-700 outline-none  border rounded-md"
             type="text"
-            value={formInput?.email}
-            name="email"
+            value={formInput?.phone}
+            name="phone"
             onChange={handleChange}
           />
-          {validator.message("email", formInput?.email, "required|email")}
-          <ShowError data={errors.email} />
+          {validator.message("phone", formInput?.phone, "required|number")}
+          <ShowError data={errors.phone} />
         </div>
 
-        {/* Password */}
-        <div className="mt-4">
-          <div className="flex justify-between">
-            <label
-              className="block mb-2 text-sm font-medium  "
-              htmlFor="password">
-              Password
-            </label>
-          </div>
+        {/* otp */}
+        {isOtp && (
+          <div className="mt-4">
+            <div className="flex justify-between">
+              <label className="block mb-2 text-sm font-medium  " htmlFor="otp">
+                Otp
+              </label>
+            </div>
 
-          <div className="flex gap-2 border-gray-700 border rounded-md px-4 items-center">
-            <input
-              autoComplete="off"
-              id="password"
-              className="block w-full  py-2 outline-none"
-              type={show ? "text" : "password"}
-              value={formInput?.password}
-              name="password"
-              onChange={handleChange}
-            />
-            <span
-              onClick={() => setShow(!show)}
-              className="text-2xl cursor-pointer text-color">
-              {show ? <AiFillEyeInvisible /> : <AiFillEye />}
-            </span>
+            <div className="flex gap-2 border-gray-700 border rounded-md px-4 items-center">
+              <input
+                autoComplete="off"
+                id="otp"
+                className="block w-full  py-2 outline-none"
+                type="text"
+                value={formInput?.otp}
+                name="otp"
+                onChange={handleChange}
+              />
+            </div>
+            {validator.message("otp", formInput?.otp, "required|number")}
+            <ShowError data={errors.otp} />
           </div>
-          {validator.message("password", formInput?.password, "required")}
-          <ShowError data={errors.password} />
-        </div>
+        )}
 
         {/* Submit Button */}
         <div className="mt-6">
           <button
             type="submit"
             disabled={loading}
-            className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize  bg-color duration-300 flex items-center justify-center transform  rounded-md bg-button focus:outline-none ">
+            className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize  bg-color duration-300 flex items-center justify-center transform  rounded-md bg-button focus:outline-none "
+          >
             {loading ? <ButtonLoader /> : " Sign In"}
           </button>
         </div>
